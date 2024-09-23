1700a2820be0771e780997171d73cc3e75f08c49
kachaMukabe <kachamukabe@gmail.com>
2024-09-23 21:56:38 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 pwd
+cd /webapps/TheRook || exit 1
+pwd
 sudo git pull origin main
 /webapps/TheRook/venv/bin/pip install -r requirements.txt
-sudo systemctl restart restart
+sudo systemctl restart therook
 sudo systemctl restart nginx
