0a7a65a3cd35af86e1360e9e4e57282018319364
kachaMukabe <kachamukabe@gmail.com>
2024-09-23 21:28:59 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+pwd
 sudo git pull origin main
-source venv/bin/activate
-pip install -r requirements.txt
+/webapps/TheRook/venv/bin/pip install -r requirements.txt
 sudo systemctl restart restart
 sudo systemctl restart nginx
