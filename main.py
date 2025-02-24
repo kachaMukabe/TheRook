74f17e9e5ea6d25a1c07fa04e9123c5efbe9686b
kachaMukabe <kachamukabe@gmail.com>
2025-02-24 08:02:29 +0200

    Adding phone number to excel sheet

--- main.py
+++ main.py
@@ -244,7 +244,9 @@ def write_to_sheet(message: RapidProEmailMessage):
         logger.info("In send to sheet kinda")
         logger.info(message)
         logger.info(message.results)
-        sheet.append_row([value["value"] for key, value in message.results.items()])
+        values = [value["value"] for key, value in message.results.items()]
+        values.append(message.contact.urn)
+        sheet.append_row(values)
     except Exception as e:
         return {"status": "Failed to write to sheet", "error": str(e)}
 
